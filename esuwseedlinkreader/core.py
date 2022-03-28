32b5450b147575bea23c3e2b828c37544d975935
Nimrod Astarhan <nimastt@gmail.com>
2022-03-28 12:52:44 +0000

    emit data to socketio

--- esuwseedlinkreader/core.py
+++ esuwseedlinkreader/core.py
@@ -1,10 +1,49 @@
 # -*- coding: utf-8 -*-
 from . import helpers
 import numpy as np
+import socketio 
 from obspy.clients.seedlink.easyseedlink import EasySeedLinkClient
 
 
+
 class GsiClient(EasySeedLinkClient):
+
+    global emit_data
+    global sio 
+
+    emit_data = False
+    sio = socketio.Client()
+
+    def __init__(self, addr):
+        super().__init__(addr)
+        #self.sio = socketio.AsyncClient()\
+        sio.connect('https://earth-signal-universe-wide.space/')
+        print('my sid is', sio.sid)
+
+    @staticmethod
+    @sio.event
+    def connect():
+        print("I'm connected!")
+        emit_data = True
+    
+    @staticmethod
+    @sio.event
+    def connect_error(data):
+        print("The connection failed!")
+        emit_data = False   
+
+    @staticmethod
+    @sio.event
+    def disconnect():
+        print("I'm disconnected!")
+        emit_data = False
+
+    # @sio.on('*')
+    # async def catch_all(event, data):
+    #     print("ASYNCIO EVENT")
+    #     print(event)
+    #     print(data)
+    #     pass
     """
     Read seedlink
     """
@@ -19,7 +58,8 @@ class GsiClient(EasySeedLinkClient):
         trace.detrend(type='linear')
         div = np.true_divide(trace.data, 405966.0)
         print(div)
-
+        if (emit_data):
+            sio.emit('seis-rec', {'data': div.tolist()})
 
 def get_hmm():
     """Get a thought."""
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 from esuwseedlinkreader.core import GsiClient
-client = GsiClient('82.102.143.63:18000')
-client.select_stream('IS', 'DSI', 'ENZ')
-client.run()
+import asyncio
+
+#async def main():
+def main():
+
+  client = GsiClient('82.102.143.63:18000')
+  client.select_stream('IS', 'DSI', 'ENZ')
+  client.run()
+
+main()
+#asyncio.run(main())
